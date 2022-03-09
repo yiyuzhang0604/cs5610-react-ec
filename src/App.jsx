4bf9ed90d1fbeb1c2536fbc9ea7eeea7b42025ec
yiyuzhang0604 <yiyuzhang0604@gmail.com>
2022-03-09 15:09:32 -0800

    add file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import { useState } from 'react';
+import Page from './component/Page'; 
+
+export default function App() {
+  return (
+    <div className='page'>
+      
+    </div>
+
+  )
+  
+}
+
+
---
+++ src/component/Page.jsx
@@ -0,0 +1,25 @@
+import React from 'react'; 
+import './Page.css'; 
+
+export default function Page() {
+    return (
+        <div className='page'>
+        <div className='header'><div className='title'>Yiyu's website</div></div>
+        <div className='nav'></div>
+        <div className='main'>
+            <div className='container'>
+            <div className='box'></div>
+            </div>
+        </div>
+
+        <div className='sb'>
+
+        </div>
+
+        <div className='footer'></div>
+
+      
+    </div>
+        
+    )
+}
